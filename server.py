d17a15a87aa3b9bcfb6ae730faac3841286cb3b5
Antônio Lucena <antonio.bezerralucena@ufpe.br>
2024-03-11 21:27:41 -0300

    server.py completa

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from Rdt import *
 import time
 
-def start_serv():
+def main():
     print("\nAbrindo servidor... Por favor, aguarde 5 segundos\n")
     i = 5
     while i != 0:
@@ -9,13 +9,14 @@ def start_serv():
         time.sleep(1)
         i = i - 1
     print('0\n')
+
+    # Criando o servidor
+    server = Rdt("server",'s')
+
     print("Servidor aberto.\n\n")
 
+    server.waiting()
 
-# Criando o servidor
-server = Rdt("server",'s')
 
-start_serv()
-# print("Aberto")
-
-server.waiting()
+if __name__ == "__main__":
+    main()
